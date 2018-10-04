d8c2a51a44c7b5af86e606f6296a8be0d11bda37
adsantos97 <adsantos@loyola.edu>
2018-10-04 00:03:42 -0400

    starting view

--- app/src/main/java/com/example/arizz/simpleblackjack/MainActivity.java
+++ app/src/main/java/com/example/arizz/simpleblackjack/MainActivity.java
@@ -1,22 +1,45 @@
 package com.example.arizz.simpleblackjack;
 
-/**
- * Description of class
- * @author Arizza Santos
- * @version 1.0 10/4/2018
- */
-
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
-    private SimpleBlackjack game;
+    public final static String MA = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        game = new SimpleBlackjack("Player", "Dealer");
-        setContentView(R.layout.activity_main);
+        Configuration config = getResources().getConfiguration();
+        modifyLayout(config);
+        //setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Checks the orientation and screen dimension when
+     * the user rotates the device
+     * @param newConfig Configuration object that specifies the new
+     *                  device configuration
+     */
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Log.w(MA, "Height: " + newConfig.screenHeightDp);
+        Log.w(MA, "Width: " + newConfig.screenWidthDp);
+
+        Log.w(MA, "Orientation: " + newConfig.orientation);
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Log.w(MA, "Horizontal position.");
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            Log.w(MA, "Portrait position.");
+        } else {
+            Log.w(MA, "Undetermined position.");
+        }
+    }
+
+    public void run(View view) {
+
     }
 }
